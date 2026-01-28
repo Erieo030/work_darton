e96f5a12987910b52367e1d35d1cf2e666f23bd5
Erieo <63690552+Erieo030@users.noreply.github.com>
2026-01-28 08:22:47 +0800

    Initial commit:前端v1

---
+++ tailwind-config.js
@@ -0,0 +1,15 @@
+// <!-- 企業字型 -->
+// tailwind-config.js
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Noto Sans TC', 'system-ui'],
+        },
+        colors: {
+          'hota-green': '#008744', // 自定義和大綠
+          'hota-gray': '#525f6b',  // 自定義選單深灰
+        }
+      }
+    }
+  }
